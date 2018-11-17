16d98f47124ba5c3c6d09b65f5f7dde26d7b8dde
karol.nadratowski <karol.nadratowski@brightinventions.pl>
2018-11-17 22:31:35 +0100

    Add reusable search bar component

--- src/containers/TeamList/TeamList.js
+++ src/containers/TeamList/TeamList.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import TeamInvitation from "../../components/TeamList/Invitation/TeamInvitation";
 import TeamRequest from "../../components/TeamList/Request/TeamRequest";
-
+import Searchbar from "../../components/UI/Searchbar/Searchbar";
+import "./TeamList.css";
 class TeamList extends Component {
   state = {};
+  handleSearchbarChange = inputValue => {
+    console.log(inputValue);
+  };
   render() {
     return (
       <div>
+        <div className="SearchbarBar">
+          <Searchbar onSerchbarChanged={this.handleSearchbarChange} />
+        </div>
+
         <TeamInvitation />
         <TeamRequest />
       </div>
