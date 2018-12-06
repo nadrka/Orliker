a79038cd96cda9af6ce86f3172751c514a6a00f0
karol.nadratowski <karol.nadratowski@brightinventions.pl>
2018-12-06 16:33:52 +0100

    Add material ui design

--- src/containers/PlayerPanel/PlayerPanel.js
+++ src/containers/PlayerPanel/PlayerPanel.js
@@ -8,28 +8,74 @@ import PlayerCarrer from "../PlayerCarrer/PlayerCarrer";
 import Schedule from "../../components/Schedule/Schedule";
 import PlayerLeagueSchedule from "../PlayerLeagueSchedule/PlayerLeagueSchedule";
 import PanelOptions from "../../components/PanelOptions/PanelOptions";
+import TextField from "material-ui/TextField";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import Dialog from "material-ui/Dialog";
+import FlatButton from "material-ui/FlatButton";
+import RaisedButton from "material-ui/RaisedButton";
 import { ROUTES } from "../../utils/Constants";
 import { getData } from "../../utils/NetworkFunctions";
+
 class PlayerPanel extends Component {
-  state = { choosenOption: "schedule", player: null, team: null, playedMatches: [], upcomingMatches: [] };
+  state = {
+    choosenOption: "schedule",
+    player: null,
+    team: null,
+    playedMatches: [],
+    upcomingMatches: [],
+    open: false
+  };
   handleOptionChange = option => {
     this.setState({ choosenOption: option });
   };
 
   async componentDidMount() {
-    const player = await getData(`${ROUTES.PLAYERS}/${this.props.match.params.id}`);
+    const player = await getData(
+      `${ROUTES.PLAYERS}/${this.props.match.params.id}`
+    );
     const team = await getData(`${ROUTES.TEAMS}/${player.teamId}`);
-    const upcomingMatches = await getData(`${ROUTES.TEAMS}/${player.teamId}/matches/upcoming`);
-    const playedMatches = await getData(`${ROUTES.TEAMS}/${player.teamId}/matches/played`);
-    this.setState({ player: player, team: team, playedMatches: playedMatches, upcomingMatches: upcomingMatches });
+    const upcomingMatches = await getData(
+      `${ROUTES.TEAMS}/${player.teamId}/matches/upcoming`
+    );
+    const playedMatches = await getData(
+      `${ROUTES.TEAMS}/${player.teamId}/matches/played`
+    );
+    this.setState({
+      player: player,
+      team: team,
+      playedMatches: playedMatches,
+      upcomingMatches: upcomingMatches
+    });
   }
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
   render() {
     let choosenOption;
+    const actions = [
+      <MuiThemeProvider>
+        <FlatButton label="Cancel" primary={true} onClick={this.handleClose} />,
+        <FlatButton
+          label="Submit"
+          primary={true}
+          disabled={true}
+          onClick={this.handleClose}
+        />
+      </MuiThemeProvider>
+    ];
     switch (this.state.choosenOption) {
       case "schedule":
         choosenOption = (
-          <Schedule upcomingMatches={this.state.upcomingMatches} playedMatches={this.state.playedMatches} />
+          <Schedule
+            upcomingMatches={this.state.upcomingMatches}
+            playedMatches={this.state.playedMatches}
+          />
         );
         break;
       case "statistics":
@@ -54,6 +100,28 @@ class PlayerPanel extends Component {
             }}
           />
           {choosenOption}
+        </div>
+        <div>
+          <MuiThemeProvider>
+            <RaisedButton label="Stwórz własną" onClick={this.handleOpen} />
+            <RaisedButton
+              label="Dołącz do istniejącej"
+              onClick={this.handleOpen}
+            />
+            <Dialog
+              title="Tworzenie własnej druzyny"
+              actions={actions}
+              modal={true}
+              open={this.state.open}
+            >
+              <TextField
+                hintText="Hint Text"
+                errorText="This field is required"
+                floatingLabelText="Floating Label Text"
+              />
+              <br />
+            </Dialog>
+          </MuiThemeProvider>
         </div>
       </div>
     );
